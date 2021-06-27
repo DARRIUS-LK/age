3e22ffefb9257b510694670c9d559ea93c8e96ba
unknown <darrius.clk@gmail.com>
2021-06-27 22:40:57 +0800

    age test

---
+++ age.py
@@ -0,0 +1,22 @@
+a = input('have you drive before? (yes/no): ')
+b = input('please enter your age: ')
+
+def is_number(b):
+		try: 
+			int(b)
+			return True
+		except ValueError:
+			pass
+
+if is_number(b) == True:
+	b = int(b)
+	if a == 'yes'and b >= 18:
+		print('you passed the test')
+	elif a == 'no' and b >= 18:
+		print('you passed the test')
+	elif a == 'yes' and b < 18:
+		print('you failed the test')
+	elif a == 'no' and b < 18:
+		print('you passed the test')
+else:
+	print('Please input the number for your age info! ')
